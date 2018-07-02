e11c710a7c146b2df2b8239665d754a7e1b4d37b
Elkeno Jones <elkeno_q.jones@hotmail.com>
2018-07-02 12:21:49 -0300

    Added extra samples

--- index.php
+++ index.php
@@ -9,15 +9,33 @@ $title = "Demo";
 <body>
 	<ol>
 		<li>
-			<a href="template-no-repeat/index.html">Sample 1</a>
+			<a href="template-no-repeat/index.html">Sample 1-a</a>
 			<ul>
 				<li>No repeating background</li>
 			</ul>
 		</li>
 		<li>
-			<a href="template-repeat/index.html">Sample 2</a>
+			<a href="template-no-repeat/pool-news.html">Sample 1-b</a>
+			<ul>
+				<li>No repeating background (pool care)</li>
+			</ul>
+		</li>
+		<li>
+			<a href="template-no-repeat/pool-news-white.html">Sample 1-c</a>
+			<ul>
+				<li>No repeating background (pool care - white background)</li>
+			</ul>
+		</li>
+		<li>
+			<a href="template-repeat/index.html">Sample 2-a</a>
 			<ul>
 				<li>Repeating background</li>
+			</ul>
+		</li>
+		<li>
+			<a href="template-repeat/index-pool.html">Sample 2-b</a>
+			<ul>
+				<li>Repeating background (pool care)</li>
 			</ul>
 		</li>
 	</ol>
